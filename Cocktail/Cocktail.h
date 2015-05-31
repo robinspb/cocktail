4a2fd0554befd52b269a64cbf06b7976862f34b8
Peter B. Robinson <robinson96@llnl.gov>
2015-05-31 05:28:33 -0700

    Initial Commit

---
+++ Cocktail/Cocktail.h
@@ -0,0 +1,23 @@
+/*
+ *  Cocktail.h
+ *  Cocktail
+ *
+ *  Created by Robinson, Peter on 5/31/15.
+ *  Copyright (c) 2015 Robinson. All rights reserved.
+ *
+ */
+
+#ifndef Cocktail_
+#define Cocktail_
+
+/* The classes below are exported */
+#pragma GCC visibility push(default)
+
+class Cocktail
+{
+	public:
+		void HelloWorld(const char *);
+};
+
+#pragma GCC visibility pop
+#endif
---
+++ Cocktail/CocktailPriv.h
@@ -0,0 +1,19 @@
+/*
+ *  CocktailPriv.h
+ *  Cocktail
+ *
+ *  Created by Robinson, Peter on 5/31/15.
+ *  Copyright (c) 2015 Robinson. All rights reserved.
+ *
+ */
+
+/* The classes below are not exported */
+#pragma GCC visibility push(hidden)
+
+class CocktailPriv
+{
+	public:
+		void HelloWorldPriv(const char *);
+};
+
+#pragma GCC visibility pop
